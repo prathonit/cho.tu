d8f450131c44e1dc69c79872881a68321fb936dc
= <=>
2020-08-29 00:06:49 +0530

    MODIFIED URL function

--- shorten.php
+++ shorten.php
@@ -7,14 +7,14 @@
 		$url=new Urls;
 		#sanitizing the data 
 		$o_url=$_POST['urlToShorten'];
-		$o_url = substr($o_url, max(0, strlen($o_url) - 20), strlen($o_url)-1);
+		$o_urls = substr($o_url, max(0, strlen($o_url) - 20), strlen($o_url)-1);
 		#checking for duplicate entry in the database 
 		if ($database->checkDuplicateInDatabase($o_url)){
 			$s_url=$database->selectFromDatabaseGiveSUrl($o_url);
 			header("Location:url.php?url=$s_url");	
 		}else{
 			$id=getValidId();
-			$s_url=$url->shortenUrl($o_url);
+			$s_url=$url->shortenUrl($o_urls);
 			$date=date('d/m/Y h:i:s:a');
 			if($database->insertIntoDatabase($id,$o_url,$s_url,$date)){
 				header("Location:url.php?url=$s_url");

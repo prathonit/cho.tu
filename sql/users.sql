b9fc6fc7c06e7b3e05eb8788e3d76dd65cb87f74
ps-priyanshi <priyanshi.carmel@gmail.com>
2020-10-01 19:08:14 +0530

    Update users.sql

--- sql/users.sql
+++ sql/users.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `users` (
-	`user_email` VARCHAR(128) NOT NULL UNIQUE,
-	`user_name` VARCHAR(128) NOT NULL UNIQUE,
+	`user_email` VARCHAR(256) NOT NULL UNIQUE,
+	`user_name` VARCHAR(256) NOT NULL UNIQ,
 	`password` TEXT NOT NULL,
-	`bio` TEXT,
-	`pic` TEXT,
+	`bio` VARCHAR(128),
+	`pic` VARCHAR(512),
 	PRIMARY KEY (`user_email`)
 ) ENGINE=InnoDB;

bc6cd39da21d1301188ee7bd37cb1c4d9902ab79
ps-priyanshi <priyanshi.carmel@gmail.com>
2020-10-01 19:11:30 +0530

    Update add_post.php

--- lib/ajaxify/add_post.php
+++ lib/ajaxify/add_post.php
@@ -1,12 +1,16 @@
 <?php 
 include_once '../config.php';
 session_start();
-$post_user_email = sanitize_input($_REQUEST['user_email']);
-$post_text = sanitize_input($_REQUEST['post_text']);
-$post = new Post;
-if ($post->addPost($post_user_email, $_SESSION['user_name'], $_SESSION['user_pic'],$post_text, 0)) {
-    echo 1;
+if (isset($_REQUEST['user_email']) && isset($_REQUEST['post_text'])) {
+     $post_user_email = sanitize_input($_REQUEST['user_email']);
+     $post_text = sanitize_input($_REQUEST['post_text']);
+     $post = new Post;
+     if ($post->addPost($post_user_email, $_SESSION['user_name'], $_SESSION['user_pic'],$post_text, 0)) {
+        echo 1;
+      } else {
+           echo 0;
+    }   
 } else {
-    echo 0;
+    die("Bad request, parameters missing");
 }
 ?>
